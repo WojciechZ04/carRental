2f3ce5731e42d183024dd29560e3d792d24616e5
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-02-27 22:51:28 +0100

    background update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { AuthComponent } from './auth/auth.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { BookComponent } from './book/book.component';
-import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    AuthComponent,
     NavbarComponent,
-    BookComponent,
-    AboutComponent,
   ],
   imports: [
     BrowserModule,
