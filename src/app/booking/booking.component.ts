cef5811945278e96ef6a3106712571608ae805e5
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-03-01 15:13:25 +0100

    booking update: create grid for form, add stepper v1.0

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,8 +1,27 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css'],
 })
-export class BookingComponent {}
+export class BookingComponent {
+  currentStep = 1;
+  onNext() {
+    this.currentStep++;
+  }
+  onBack() {
+    this.currentStep--;
+  }
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = true;
+
+  constructor(private _formBuilder: FormBuilder) {}
+}
--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -1,4 +1,4 @@
-import { NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -6,11 +6,32 @@ import { CarSelectionComponent } from './car-selection/car-selection.component';
 import { ExtrasComponent } from './extras/extras.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
 import { PaymentComponent } from './payment/payment.component';
-import { BookingRoutingModule } from './booking-routing.module';
 import { BookingComponent } from './booking.component';
+import { BookingRoutingModule } from './booking-routing.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [BookingComponent, CarSelectionComponent, ExtrasComponent, ContactDetailsComponent, PaymentComponent],
-  imports: [CommonModule, RouterModule, BookingRoutingModule],
+  declarations: [
+    BookingComponent,
+    CarSelectionComponent,
+    ExtrasComponent,
+    ContactDetailsComponent,
+    PaymentComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BookingRoutingModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class BookingModule {}
