5addeda7896bd07b99b9023eb245443195db26af
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-04-14 21:55:12 +0200

    add icons

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
 
-import { CarsComponent } from "./cars.component";
+import { CarsComponent } from './cars.component';
 
 @NgModule({
-	declarations: [CarsComponent],
-	imports: [
-		CommonModule,
-		MatCardModule,
-		MatPaginatorModule,
-		RouterModule.forChild([
-			{ path: '', component: CarsComponent }
-		])
-	]
+  declarations: [CarsComponent],
+  imports: [
+	MatIconModule,
+    CommonModule,
+    MatCardModule,
+    MatPaginatorModule,
+    RouterModule.forChild([{ path: '', component: CarsComponent }]),
+  ],
 })
 export class CarsModule {}
